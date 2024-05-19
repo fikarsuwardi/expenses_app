627f30a6aa046c9cea375ace2ff790c0c182c5ec
fikarsuwardi <fikarsuwardi@gmail.com>
2024-05-19 14:24:58 +0700

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Expenses App
+
+## Recent
+![recent](/assets/ss/recent.png "recent")
+## All
+![all](/assets/ss/all.png "all")
+## Add
+![add](/assets/ss/add.png "add")
--- util/http.js
+++ util/http.js
@@ -1,6 +1,44 @@
 import axios from 'axios';
 
-const BACKEND_URL = "https://expenses-app-34eae-default-rtdb.asia-southeast1.firebasedatabase.app";
+const BACKEND_URL = "https://expenses-app-34eae-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+// export async function storeExpense(expenseData) {
+//   const response = await fetch(`${BACKEND_URL}/expenses.json`, {
+//     method: 'POST',
+//     body: JSON.stringify(expenseData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+//   const data = await response.json();
+ 
+//   const id = data.name;
+//   return id;
+// }
+
+// export async function fetchExpenses() {
+//     const response = await fetch(`${BACKEND_URL}/expenses.json`);
+//     const data = await response.json();
+   
+//     return Object.entries(data).map(([id, expenseData]) => {
+//       return { id: id, ...expenseData, date: new Date(expenseData.date) };
+//     });
+//   }
+   
+//   export function updateExpense(id, expenseData) {
+//     return fetch(`${BACKEND_URL}/expenses/${id}.json`, {
+//       method: 'PUT',
+//       body: JSON.stringify(expenseData),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+//   }
+//   export function deleteExpense(id) {
+//     return fetch(`${BACKEND_URL}/expenses/${id}.json`, {
+//       method: 'DELETE',
+//     });
+//   }
 
 export async function storeExpense(expenseData) {
   const response = await axios.post(BACKEND_URL + '/expenses.json', expenseData);
@@ -10,6 +48,7 @@ export async function storeExpense(expenseData) {
 
 export async function fetchExpenses() {
   const response = await axios.get(BACKEND_URL + '/expenses.json');
+
   const expenses = [];
 
   for (const key in response.data) {
